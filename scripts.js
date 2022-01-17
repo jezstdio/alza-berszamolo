4602840313bec5abcf25df21f4c559aef8d4ad19
jezstdio <jezstdio@protonmail.com>
2022-01-17 23:49:06 +0100

    add default salary option

--- scripts.js
+++ scripts.js
@@ -2,7 +2,13 @@ const serviceInput = document.getElementById("service");
 const ironInput = document.getElementById("iron");
 const activityInput = document.getElementById("activity");
 const npsInput = document.getElementById("nps");
-const defaultGross = 265000;
+const baseSalarySelect = document.getElementById("base_salary--select");
+const baseSalaryInput = document.getElementById("base_salary--input");
+let defaultGross = 265000;
+
+function inputCheck(value) {
+    return parseInt(value.toString().replace(/\s/g, "").trim()) || 0;
+}
 
 function limitMaxScore(currentScore, maxScore) {
     if (currentScore > maxScore) {
@@ -16,6 +22,7 @@ function showGross() {
     const sum = document.getElementById("sum");
     
     sum.innerHTML = Math.round(defaultGross + service() + iron() + activity() + nps()).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + " Ft";
+    console.log(defaultGross, service(), iron(), activity(), nps());
 }
 
 function czkToHuf(czk) {
@@ -25,14 +32,14 @@ function czkToHuf(czk) {
 }
 
 function service() {
-    const value = serviceInput.value;
+    const value = inputCheck(serviceInput.value);
     const variable = 13.5;
 
     return czkToHuf(value / 100 * variable);
 }
 
 function iron() {
-    const value = ironInput.value;
+    const value = inputCheck(ironInput.value);
     const variable = 3.2;
 
     return czkToHuf(value / 100 * variable);
@@ -43,7 +50,7 @@ function activity() {
     const maxScore = targetScore * 2;
     const gross = 14625;
     const variable = 39;
-    let currentScore = limitMaxScore(activityInput.value, maxScore);
+    let currentScore = limitMaxScore(inputCheck(activityInput.value), maxScore);
 
     return ((currentScore - targetScore) * variable) + gross;
 }
@@ -62,10 +69,31 @@ serviceInput.onkeyup = showGross;
 ironInput.onkeyup = showGross;
 activityInput.onkeyup = showGross;
 npsInput.onkeyup = showGross;
+baseSalaryInput.onkeyup = e => {
+    defaultGross = inputCheck(e.currentTarget.value);
+    showGross(e);
+}
 
 serviceInput.onfocus = e => e.currentTarget.select();
 ironInput.onfocus = e => e.currentTarget.select();
 activityInput.onfocus = e => e.currentTarget.select();
 npsInput.onfocus = e => e.currentTarget.select();
+baseSalaryInput.onfocus = e => e.currentTarget.select();
+
+baseSalarySelect.onchange = e => {
+    if (e.currentTarget.value === "personal") {
+        baseSalaryInput.classList.remove("none");
+        if (!baseSalaryInput.value) {
+            baseSalaryInput.value = defaultGross;
+        } else {
+            defaultGross = inputCheck(baseSalaryInput.value);
+        }
+    } else {
+        baseSalaryInput.classList.add("none");
+        defaultGross = inputCheck(e.currentTarget.value);
+    }
+
+    showGross(e);
+}
 
 // Gross to net converter.
